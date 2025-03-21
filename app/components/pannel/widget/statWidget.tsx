46508f1e928ceca5c4d5796af86c25de388a22df
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-21 17:41:31 +0900

    style: Modify colors

--- app/components/pannel/widget/statWidget.tsx
+++ app/components/pannel/widget/statWidget.tsx
@@ -22,7 +22,7 @@ const StatWidget = ({
   colors = ["#4CAF50", "#f5f251", "#fc5353"],
   className,
 }: StatWidgetProps) => {
-  // ✅ value에 맞는 색상을 결정하는 함수
+  // value에 맞는 색상을 결정하는 함수
   const getColorByValue = (value: number): string => {
     for (let i = 0; i < thresholds.length; i++) {
       if (value < thresholds[i]) {
@@ -34,13 +34,13 @@ const StatWidget = ({
 
   const activeColor = getColorByValue(value);
 
-  // ✅ 최대값을 기준으로 퍼센트 변환
+  // 최대값을 기준으로 퍼센트 변환
   const percentage = (value / maxValue) * 100;
 
   const data = {
     datasets: [
       {
-        // ✅ maxValue 기준으로 퍼센트 변환
+        // maxValue 기준으로 퍼센트 변환
         data: [percentage, 100 - percentage],
         backgroundColor: [activeColor, "#E0E0E0"],
         borderWidth: 0,
@@ -71,17 +71,21 @@ const StatWidget = ({
 
   return (
     <div
-      className={`${className} relative flex flex-col items-center justify-center
-      border-navy-border pb-3 w-72 h-48 rounded-md shadow-lg bg-white`}
+      className={`${className} border border-modern-border relative flex flex-col items-center justify-center
+      border-navy-border pb-3 w-72 h-48 rounded-md shadow-lg bg-modern-bg`}
     >
-      {/* ✅ 라벨을 차트 위에 배치 */}
-      <div className="absolute top-2 text-lg font-bold text-text">{label}</div>
+      {/* 라벨을 차트 위에 배치 */}
+      <div className="absolute top-2 text-lg font-bold text-modern-primary">
+        {label}
+      </div>
 
       {/* 도넛 차트 */}
       <Doughnut data={data} options={options} />
 
-      {/* ✅ 차트 중앙에 값 배치 */}
-      <div className="absolute text-3xl font-bold text-text mt-8">{value}</div>
+      {/* 차트 중앙에 값 배치 */}
+      <div className="absolute text-3xl font-bold text-modern-primary mt-8">
+        {value}
+      </div>
     </div>
   );
 };
