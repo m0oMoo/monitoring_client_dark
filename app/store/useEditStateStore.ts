87a50082636b16c9771bef2ad72a9a87d1b1f387
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-31 09:10:21 +0900

    feat: Update useEditStateStore.ts

--- app/store/useEditStateStore.ts
+++ app/store/useEditStateStore.ts
@@ -9,11 +9,13 @@ export interface EditState {
  * @param editStates 대시보드 ID를 키로 가지는 객체로, 각 대시보드의 edit 상태를 저장.
  * @param setEditState 특정 대시보드의 edit 상태를 설정.
  * @param clearEditState 특정 대시보드의 상태를 제거 (예: 페이지 이동 시 상태 초기화).
+ * @param resetAllEditStates 모든 대시보드의 상태를 초기화
  */
 export interface EditStateStore {
   editStates: EditState;
   setEditState: (dashboardId: string, isEditing: boolean) => void;
   clearEditState: (dashboardId: string) => void;
+  resetAllEditStates: () => void;
 }
 
 export const useEditStateStore = create<EditStateStore>()(
@@ -33,6 +35,17 @@ export const useEditStateStore = create<EditStateStore>()(
             const newStates = { ...state.editStates };
             delete newStates[dashboardId];
             return { editStates: newStates };
+          }),
+        resetAllEditStates: () =>
+          set((state: EditStateStore) => {
+            const resetStates: EditState = Object.keys(state.editStates).reduce(
+              (acc, key) => {
+                acc[key] = false;
+                return acc;
+              },
+              {} as EditState
+            );
+            return { editStates: resetStates };
           }),
       }),
       {
