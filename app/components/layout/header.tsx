26a8a0a0ab8faafc8b4f9baf7d6e318df49c1f39
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-04-18 11:18:49 +0900

    feat: Apply setting button

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,24 +1,41 @@
 import { Home, User, Settings, Menu } from "lucide-react";
+import { useThemeStore } from "@/store/useThemeStore";
+import { useState } from "react";
+import SettingsButton from "../button/settingBtn";
 
 const Header = ({ toggleMenu }: { toggleMenu: () => void }) => {
-  return (
-    <header
-      className="w-full bg-modern-bg p-1 shadow-md flex justify-between items-center
-    fixed top-0 left-0 right-0 z-50 border-b border-modern-border"
-    >
-      <div className="flex items-center space-x-2">
-        <button onClick={toggleMenu} className="p-2">
-          <Menu className="w-5 h-5 text-ivory-text" />
-        </button>
-        <Home className="w-5 h-5 text-ivory-text" />
-        <span className="text-lg font-semibold text-ivory-text">Dashboard</span>
-      </div>
+  const { setTheme } = useThemeStore();
+  const [open, setOpen] = useState(false);
 
-      <div className="flex items-center text-ivory-text space-x-4">
-        <User className="w-5 h-5 cursor-pointer" />
-        <Settings className="w-5 h-5 cursor-pointer" />
-      </div>
-    </header>
+  const handleThemeChange = (
+    theme: "modern" | "blue" | "pink" | "orange" | "ivory"
+  ) => {
+    setTheme(theme);
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <header
+        className="w-full bg-modern-bg p-1 shadow-md flex justify-between items-center
+        fixed top-0 left-0 right-0 z-50 border-b border-modern-border"
+      >
+        <div className="flex items-center space-x-2">
+          <button onClick={toggleMenu} className="p-2">
+            <Menu className="w-5 h-5 text-ivory-text" />
+          </button>
+          <Home className="w-5 h-5 text-ivory-text" />
+          <span className="text-lg font-semibold text-ivory-text">
+            Dashboard
+          </span>
+        </div>
+
+        <div className="flex items-center text-ivory-text space-x-4">
+          <User className="w-5 h-5 cursor-pointer" />
+          <SettingsButton />
+        </div>
+      </header>
+    </>
   );
 };
 
