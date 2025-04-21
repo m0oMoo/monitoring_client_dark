60266b2285a28ff764aede5265685b42bfb231d7
yeoniii20 <commetoi_yeoni@naver.com>
2025-04-22 00:48:06 +0900

    fix: Hidden API data binding tab

--- app/(monitoring)/d/content/dataBinding.tsx
+++ app/(monitoring)/d/content/dataBinding.tsx
@@ -170,187 +170,264 @@ const DataBinding = () => {
   return (
     <div
       className="bg-modern-bg border-l border-0.5 no-scrollbar border-modern-border
-    pl-6 py-8 w-[300px] h-[100vh] overflow-y-auto"
+  pl-6 py-8 w-[300px] h-[100vh] overflow-y-auto"
     >
       <div className="w-full flex flex-col">
         <h2 className="text-lg font-semibold mb-4 text-modern-text">
           Data Binding
         </h2>
-
-        {/* API vs Query 선택 토글 */}
+        {/* 테이블 선택 */}
         <div className="flex flex-col mb-6">
-          <label className="text-sm2 text-modern-text mb-2">
-            Select Binding Method
-          </label>
-          <RoundToggleBtnGroup
-            options={["API", "Query"]}
-            selected={isApiBinding}
-            onChange={setIsApiBinding}
-            label=""
-            clssName="w-[120px]"
+          <label className="text-sm2 text-modern-text mb-2">Select Table</label>
+          <Dropdown
+            value={selectedTable || ""}
+            onChange={handleTableChange}
+            options={Object.keys(COLUMNS).map((table) => ({
+              label: table,
+              value: table,
+            }))}
+            placeholder="Select a table"
+            className="w-[250px]"
           />
         </div>
 
-        {/* API Binding 옵션 */}
-        {isApiBinding === "API" ? (
-          <>
-            <div className="flex flex-col mb-6" style={{ marginBottom: 24 }}>
-              <label className="mb-2 text-sm2 text-modern-text">
-                Generated URL
-              </label>
-              <div className="p-2 text-modern-point bg-modern-point_20 rounded text-sm break-all w-[250px]">
-                {apiUrl || "Please select both Target and Metric."}
-              </div>
-            </div>
+        {/* 컬럼 선택 */}
+        {selectedTable && (
+          <div className="flex flex-col mb-6">
+            <label className="text-sm2 text-modern-text mb-2">
+              {selectedSection === "chartOption"
+                ? "Select Columns"
+                : "Select Column"}
+            </label>
 
-            {/* Target 선택 */}
-            <div className="flex flex-col mb-6" style={{ marginBottom: 24 }}>
-              <label className="mb-2 text-sm2 text-modern-text">
-                * Select Target
-              </label>
-              <Dropdown
-                value={selectedTarget}
-                onChange={(value) => {
-                  setSelectedTarget(value);
-                  setSelectedMetric([]);
-                }}
-                options={TARGET_OPTIONS}
-                className="w-[250px]"
-              />
-            </div>
-
-            {/* Metric 선택 */}
-            <div className="flex flex-col mb-6" style={{ marginBottom: 24 }}>
-              <label className="mb-2 text-sm2 text-modern-text">
-                * Select Metric
-              </label>
+            {selectedSection === "chartOption" ? (
               <MultiSelectDropdown
-                value={selectedMetric}
-                onChange={setSelectedMetric}
-                options={metricOptionsMap[selectedTarget] || []}
-                placeholder="Select one or more columns"
-                className="w-[250px] min-h-8"
-              />
-            </div>
-            <div className="flex flex-col mb-6" style={{ marginBottom: 24 }}>
-              <label className="mb-2 text-sm2 text-modern-text">
-                Query Parameters (Optional)
-              </label>
-              <TextInput
-                value={queryParams}
-                onChange={(e) => setQueryParams(e)}
-                placeholder="e.g., id=1&name=John"
-                className="w-[250px]"
-              />
-            </div>
-
-            <LargeBtn
-              title={" Fetch Data"}
-              onClick={() => {
-                console.log("Fetching from URL:", generatedUrl);
-              }}
-              disabled={!selectedTarget || selectedMetric.length === 0}
-            />
-            {/* API URL 입력 */}
-            {/* <div className="flex flex-col mb-6">
-              <label className="text-sm2 text-modern-text mb-2">API URL</label>
-              <TextInput
-                value={apiUrl}
-                onChange={(e) => setApiUrl(e)}
-                placeholder="Enter API URL"
-                className="w-[250px]"
-              />
-            </div> */}
-
-            {/* Query parameters */}
-            {/* <div className="flex flex-col mb-6">
-              <label className="text-sm2 text-modern-text mb-2">
-                Query Parameters (Optional)
-              </label>
-              <TextInput
-                value={queryParams}
-                onChange={(e) => setQueryParams(e)}
-                placeholder="e.g., id=1&name=John"
-                className="w-[250px]"
-              />
-            </div> */}
-          </>
-        ) : (
-          <>
-            {/* 테이블 선택 */}
-            <div className="flex flex-col mb-6">
-              <label className="text-sm2 text-modern-text mb-2">
-                Select Table
-              </label>
-              <Dropdown
-                value={selectedTable || ""}
-                onChange={handleTableChange}
-                options={Object.keys(COLUMNS).map((table) => ({
-                  label: table,
-                  value: table,
+                value={selectedColumns}
+                onChange={setSelectedColumns}
+                options={COLUMNS[selectedTable].map((column) => ({
+                  label: column,
+                  value: column,
                 }))}
-                placeholder="Select a table"
+                placeholder="Select one or more columns"
                 className="w-[250px]"
               />
-            </div>
-
-            {/* 컬럼 선택 */}
-            {selectedTable && (
-              <div className="flex flex-col mb-6">
-                <label className="text-sm2 text-modern-text mb-2">
-                  {selectedSection === "chartOption"
-                    ? "Select Columns"
-                    : "Select Column"}
-                </label>
-
-                {selectedSection === "chartOption" ? (
-                  <MultiSelectDropdown
-                    value={selectedColumns}
-                    onChange={setSelectedColumns}
-                    options={COLUMNS[selectedTable].map((column) => ({
-                      label: column,
-                      value: column,
-                    }))}
-                    placeholder="Select one or more columns"
-                    className="w-[250px]"
-                  />
-                ) : (
-                  <Dropdown
-                    value={selectedColumn || ""}
-                    onChange={setSelectedColumn}
-                    options={COLUMNS[selectedTable].map((column) => ({
-                      label: column,
-                      value: column,
-                    }))}
-                    placeholder="Select a column"
-                    className="w-[250px]"
-                  />
-                )}
-              </div>
+            ) : (
+              <Dropdown
+                value={selectedColumn || ""}
+                onChange={setSelectedColumn}
+                options={COLUMNS[selectedTable].map((column) => ({
+                  label: column,
+                  value: column,
+                }))}
+                placeholder="Select a column"
+                className="w-[250px]"
+              />
             )}
-            {/* SQL 쿼리 입력 */}
-            <div className="flex flex-col mb-6">
-              <label className="text-sm2 text-modern-text mb-2">
-                Write Query
-              </label>
-              <TextArea
-                value={query}
-                onChange={(value) => setQuery(value)}
-                placeholder="SELECT * FROM table WHERE ..."
-                className="w-[250px]"
-              />
-            </div>
-
-            {/* 쿼리 실행 버튼 */}
-            <LargeBtn
-              title={"Execute Query"}
-              onClick={handleQueryExecute}
-              disabled={!query.trim()}
-            />
-          </>
+          </div>
         )}
+        {/* SQL 쿼리 입력 */}
+        <div className="flex flex-col mb-6">
+          <label className="text-sm2 text-modern-text mb-2">Write Query</label>
+          <TextArea
+            value={query}
+            onChange={(value) => setQuery(value)}
+            placeholder="SELECT * FROM table WHERE ..."
+            className="w-[250px]"
+          />
+        </div>
+
+        {/* 쿼리 실행 버튼 */}
+        <LargeBtn
+          title={"Execute Query"}
+          onClick={handleQueryExecute}
+          disabled={!query.trim()}
+        />
       </div>
     </div>
+
+    // <div
+    //   className="bg-modern-bg border-l border-0.5 no-scrollbar border-modern-border
+    // pl-6 py-8 w-[300px] h-[100vh] overflow-y-auto"
+    // >
+    //   <div className="w-full flex flex-col">
+    //     <h2 className="text-lg font-semibold mb-4 text-modern-text">
+    //       Data Binding
+    //     </h2>
+
+    //     {/* API vs Query 선택 토글 */}
+    //     <div className="flex flex-col mb-6">
+    //       <label className="text-sm2 text-modern-text mb-2">
+    //         Select Binding Method
+    //       </label>
+    //       <RoundToggleBtnGroup
+    //         options={["API", "Query"]}
+    //         selected={isApiBinding}
+    //         onChange={setIsApiBinding}
+    //         label=""
+    //         clssName="w-[120px]"
+    //       />
+    //     </div>
+
+    //     {/* API Binding 옵션 */}
+    //     {isApiBinding === "API" ? (
+    //       <>
+    //         <div className="flex flex-col mb-6" style={{ marginBottom: 24 }}>
+    //           <label className="mb-2 text-sm2 text-modern-text">
+    //             Generated URL
+    //           </label>
+    //           <div className="p-2 text-modern-point bg-modern-point_20 rounded text-sm break-all w-[250px]">
+    //             {apiUrl || "Please select both Target and Metric."}
+    //           </div>
+    //         </div>
+
+    //         {/* Target 선택 */}
+    //         <div className="flex flex-col mb-6" style={{ marginBottom: 24 }}>
+    //           <label className="mb-2 text-sm2 text-modern-text">
+    //             * Select Target
+    //           </label>
+    //           <Dropdown
+    //             value={selectedTarget}
+    //             onChange={(value) => {
+    //               setSelectedTarget(value);
+    //               setSelectedMetric([]);
+    //             }}
+    //             options={TARGET_OPTIONS}
+    //             className="w-[250px]"
+    //           />
+    //         </div>
+
+    //         {/* Metric 선택 */}
+    //         <div className="flex flex-col mb-6" style={{ marginBottom: 24 }}>
+    //           <label className="mb-2 text-sm2 text-modern-text">
+    //             * Select Metric
+    //           </label>
+    //           <MultiSelectDropdown
+    //             value={selectedMetric}
+    //             onChange={setSelectedMetric}
+    //             options={metricOptionsMap[selectedTarget] || []}
+    //             placeholder="Select one or more columns"
+    //             className="w-[250px] min-h-8"
+    //           />
+    //         </div>
+    //         <div className="flex flex-col mb-6" style={{ marginBottom: 24 }}>
+    //           <label className="mb-2 text-sm2 text-modern-text">
+    //             Query Parameters (Optional)
+    //           </label>
+    //           <TextInput
+    //             value={queryParams}
+    //             onChange={(e) => setQueryParams(e)}
+    //             placeholder="e.g., id=1&name=John"
+    //             className="w-[250px]"
+    //           />
+    //         </div>
+
+    //         <LargeBtn
+    //           title={" Fetch Data"}
+    //           onClick={() => {
+    //             console.log("Fetching from URL:", generatedUrl);
+    //           }}
+    //           disabled={!selectedTarget || selectedMetric.length === 0}
+    //         />
+    //         {/* API URL 입력 */}
+    //         {/* <div className="flex flex-col mb-6">
+    //           <label className="text-sm2 text-modern-text mb-2">API URL</label>
+    //           <TextInput
+    //             value={apiUrl}
+    //             onChange={(e) => setApiUrl(e)}
+    //             placeholder="Enter API URL"
+    //             className="w-[250px]"
+    //           />
+    //         </div> */}
+
+    //         {/* Query parameters */}
+    //         {/* <div className="flex flex-col mb-6">
+    //           <label className="text-sm2 text-modern-text mb-2">
+    //             Query Parameters (Optional)
+    //           </label>
+    //           <TextInput
+    //             value={queryParams}
+    //             onChange={(e) => setQueryParams(e)}
+    //             placeholder="e.g., id=1&name=John"
+    //             className="w-[250px]"
+    //           />
+    //         </div> */}
+    //       </>
+    //     ) : (
+    //       <>
+    //         {/* 테이블 선택 */}
+    //         <div className="flex flex-col mb-6">
+    //           <label className="text-sm2 text-modern-text mb-2">
+    //             Select Table
+    //           </label>
+    //           <Dropdown
+    //             value={selectedTable || ""}
+    //             onChange={handleTableChange}
+    //             options={Object.keys(COLUMNS).map((table) => ({
+    //               label: table,
+    //               value: table,
+    //             }))}
+    //             placeholder="Select a table"
+    //             className="w-[250px]"
+    //           />
+    //         </div>
+
+    //         {/* 컬럼 선택 */}
+    //         {selectedTable && (
+    //           <div className="flex flex-col mb-6">
+    //             <label className="text-sm2 text-modern-text mb-2">
+    //               {selectedSection === "chartOption"
+    //                 ? "Select Columns"
+    //                 : "Select Column"}
+    //             </label>
+
+    //             {selectedSection === "chartOption" ? (
+    //               <MultiSelectDropdown
+    //                 value={selectedColumns}
+    //                 onChange={setSelectedColumns}
+    //                 options={COLUMNS[selectedTable].map((column) => ({
+    //                   label: column,
+    //                   value: column,
+    //                 }))}
+    //                 placeholder="Select one or more columns"
+    //                 className="w-[250px]"
+    //               />
+    //             ) : (
+    //               <Dropdown
+    //                 value={selectedColumn || ""}
+    //                 onChange={setSelectedColumn}
+    //                 options={COLUMNS[selectedTable].map((column) => ({
+    //                   label: column,
+    //                   value: column,
+    //                 }))}
+    //                 placeholder="Select a column"
+    //                 className="w-[250px]"
+    //               />
+    //             )}
+    //           </div>
+    //         )}
+    //         {/* SQL 쿼리 입력 */}
+    //         <div className="flex flex-col mb-6">
+    //           <label className="text-sm2 text-modern-text mb-2">
+    //             Write Query
+    //           </label>
+    //           <TextArea
+    //             value={query}
+    //             onChange={(value) => setQuery(value)}
+    //             placeholder="SELECT * FROM table WHERE ..."
+    //             className="w-[250px]"
+    //           />
+    //         </div>
+
+    //         {/* 쿼리 실행 버튼 */}
+    //         <LargeBtn
+    //           title={"Execute Query"}
+    //           onClick={handleQueryExecute}
+    //           disabled={!query.trim()}
+    //         />
+    //       </>
+    //     )}
+    //   </div>
+    // </div>
   );
 };
 
