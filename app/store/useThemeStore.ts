74f733017a735b15fa1f18462d706f25c8fe1f19
yeoniii20 <commetoi_yeoni@naver.com>
2025-04-22 01:46:23 +0900

    feat: Add persist

--- app/store/useThemeStore.ts
+++ app/store/useThemeStore.ts
@@ -1,11 +1,19 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface ThemeState {
   theme: "modern" | "blue" | "pink" | "orange" | "ivory";
   setTheme: (theme: ThemeState["theme"]) => void;
 }
 
-export const useThemeStore = create<ThemeState>((set) => ({
-  theme: "modern",
-  setTheme: (theme) => set({ theme }),
-}));
+export const useThemeStore = create<ThemeState>()(
+  persist(
+    (set) => ({
+      theme: "modern",
+      setTheme: (theme) => set({ theme }),
+    }),
+    {
+      name: "theme-storage",
+    }
+  )
+);
